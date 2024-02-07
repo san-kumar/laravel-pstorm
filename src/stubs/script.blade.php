648854c783b2ffb8e5f035b499dea9f30c687d15
Monta <74559702+monta42@users.noreply.github.com>
2024-02-07 22:01:29 +0100

    small fixes for bootstrap

--- src/stubs/script.blade.php
+++ src/stubs/script.blade.php
@@ -6,15 +6,15 @@
 
             let newTab = `
         <div class="phpdebugbar-dropdown m-0 p-0" style="position: relative;">
-            <a class="phpdebugbar-tab" type="button" id="dropdownMenuButton1" href="#"> PHPSTORM </a>
+            <a class="phpdebugbar-tab text-dark fw-bold" type="button" id="dropdownMenuButton1" href="#"> PHPSTORM </a>
             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" style="width: 300px; height: 200px; overflow: auto">
                 <li class="p-2 bg-light text-uppercase font-weight-bold" style="font-weight: bold; text-transform: uppercase">Views</li>
                 @foreach($views as $view)
             <li><a class="dropdown-item px-3" href="{{ $view['url'] }}">{{ $view['name'] }}</a></li>
                 @endforeach
 
-            <li class="p-3 bg-light fw-bold" style="font-weight: bold; text-transform: uppercase">Controller</li>
-            <li style="padding-bottom: 20px"><a class="dropdown-item px-3" href="{{ $controller['url'] }}">{{ $controller['name'] }}</a></li>
+            <li class="p-2 bg-light fw-bold" style="font-weight: bold; text-transform: uppercase">Controller</li>
+            <li style="padding-bottom: 10px"><a class="dropdown-item px-3" href="{{ $controller['url'] }}">{{ $controller['name'] }}</a></li>
             </ul>
         </div>
 
@@ -34,7 +34,6 @@
 
         .phpdebugbar-dropdown ul li a:hover {
             font-weight: 900 !important;
-            color: #333333 !important;
         }
 
         .phpdebugbar-dropdown:hover ul {
